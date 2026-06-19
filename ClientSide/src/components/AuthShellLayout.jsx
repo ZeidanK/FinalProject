f90625198e47ada904038e7a73713cf9c0ccc9b0
Mohammad Noor Abuasbe <noor2000asbe@gmail.com>
2026-06-19 14:14:08 +0300

    fixed the width in the login/signup pages

--- ClientSide/src/components/AuthShellLayout.jsx
+++ ClientSide/src/components/AuthShellLayout.jsx
@@ -22,7 +22,7 @@ export default function AuthShellLayout({ chipLabel, children }) {
       }}
     >
       <Container
-        maxWidth={false}
+        maxWidth="md"
         disableGutters
         sx={{ px: { xs: 2, sm: 3, md: 4, xl: 5 }, width: '100%' }}
       >
